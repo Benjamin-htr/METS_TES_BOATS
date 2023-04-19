abf65b895b9dbe13169d08d064e325e1a45292fe
Benjamin Hautier <benjamin.htr42@gmail.com>
2023-04-19 11:47:46 +0200

    finish schema prisma v1

--- packages/main-api/src/_app.ts
+++ packages/main-api/src/_app.ts
@@ -1,5 +1,4 @@
-import { router, publicProcedure } from "./lib/trpc";
-import { z } from "zod";
+import { publicProcedure, router } from "./lib/trpc";
 const appRouter = router({
   // Create publicProcedure at path 'hello'
   hello: publicProcedure.query(() => {
