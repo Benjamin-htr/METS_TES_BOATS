4e59bcda28af0be1f369c47a43458cf3cc83ea2e
Benjamin Hautier <benjamin.htr42@gmail.com>
2023-04-22 18:29:05 +0200

    erase log

--- packages/main-front/src/utils/isAuth.tsx
+++ packages/main-front/src/utils/isAuth.tsx
@@ -1,9 +1,8 @@
 import { getCookie } from "./getCookie";
 
 export const isAuth = () => {
-  const access_token = getCookie("logged_in");
-  console.log(access_token);
-  if (access_token) {
+  const logged_in = getCookie("logged_in");
+  if (logged_in) {
     return true;
   }
   return false;
